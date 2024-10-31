2cb0ae5208e7cf4f92510661c91aced20d9d2081
Jonathan Lapian <s22310315@student.unklab.ac.id>
2024-10-31 09:23:13 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Hello />
+    </div>
+  );
+};
+
+const Hello = () => {
+  return <h2>Hello React</h2>;
+};
+
+export default App;
