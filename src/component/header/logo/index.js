f34bd57b43051e638c8c59fcae784d9a4ff6dfe0
Jonathan Lapian <s22310315@student.unklab.ac.id>
2024-11-03 16:18:29 +0800

    export logo

--- src/component/header/logo/index.js
+++ src/component/header/logo/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const logo = () => {
-    return(
-      <div>
-        <h1>logo</h1>
-      </div>
-    );
+  return (
+    <div>
+      <h1>logo</h1>
+    </div>
+  );
 };
+
+export default logo;
