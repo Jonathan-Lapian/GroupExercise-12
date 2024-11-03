7493aff930646f7bdafdc0b03a8cb2998a813088
Jonathan Lapian <s22310315@student.unklab.ac.id>
2024-11-03 16:29:40 +0800

    modified some import and export

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Footer from "./component/footer";
-
+import { Logo, Gen1, Gen2, Gen3, Gen4 } from "./component/header";
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <Logo />
       <Footer />
     </div>
   );
---
+++ src/component/header/index.js
@@ -0,0 +1,4 @@
+import Logo from "./logo";
+import { Gen1, Gen2, Gen3, Gen4 } from "./generation";
+
+export { Logo, Gen1, Gen2, Gen3, Gen4 };
