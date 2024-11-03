d9d54b84d03a9b516102745fdbebb68c03e7f878
Howard2510 <S22210550@student.unklab.ac.id>
2024-11-03 15:54:56 +0800

    logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1>App.js</h1>
       <Hello />
     </div>
   );
 };
 
-const Hello = () => {
-  return <h2>Hello React</h2>;
-};
 
 export default App;
