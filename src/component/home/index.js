29641e817bcc0af6d3452248aa3ffd829abec973
Rodricko Wullur <S22210265.student.unklab.ac.id>
2024-11-05 00:36:31 +0800

    inside search

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -1,4 +1,4 @@
 import Search from "./search";
 import { Evolution, Logo, typeEffect } from "./card-info";
 
-export { Search, Evolution, Logo, typeEffect };
+export { Search, Evolution, Logo, typeEffect }
